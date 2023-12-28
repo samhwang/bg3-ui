db5f93a63bd5a64fcc9437f159ecd734821d9726
Sam Huynh <samhwang2112.dev@gmail.com>
2023-12-29 04:41:10 +1100

    feat: add playground global style

--- playgrounds/react/panda.config.ts
+++ playgrounds/react/panda.config.ts
@@ -1,19 +1,29 @@
-import { defineConfig } from '@pandacss/dev';
+import { defineConfig, defineGlobalStyles } from '@pandacss/dev';
+
+const globalStyle = defineGlobalStyles({
+  body: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+    backgroundColor: '#070606',
+    fontFamily: "'PT Serif', serif",
+    color: '#fff',
+  },
+});
 
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
 
   // Where to look for your css declarations
-  include: [
-    '../../../node_modules/@samhwang/bg3-styled-system/dist/panda.buildinfo.json',
-    './src/**/*.{js,jsx,ts,tsx}'
-  ],
+  include: ['../../../node_modules/@samhwang/bg3-styled-system/dist/panda.buildinfo.json', './src/**/*.{js,jsx,ts,tsx}'],
 
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
+  globalCss: globalStyle,
   theme: {
     extend: {},
   },
